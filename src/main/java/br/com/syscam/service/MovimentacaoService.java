15062d86aaac2751e74ac8fa2261be52d8b07e97
antonio <antoniofilhonerde@gmail.com>
2019-08-08 20:34:45 -0300

    metodo de busca por intervalo de data no service

--- src/main/java/br/com/syscam/service/MovimentacaoService.java
+++ src/main/java/br/com/syscam/service/MovimentacaoService.java
@@ -6,8 +6,13 @@ import br.com.syscam.repository.MovimentacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -60,6 +65,17 @@ public class MovimentacaoService {
             }
         }catch (Exception e){
             return false;
+        }
+    }
+
+    public List<Movimentacao> buscarPorIntervalo(String inicio, String fim) throws ParseException {
+        DateTimeFormatter formato= DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dateInicio = LocalDate.parse(inicio,formato);
+        LocalDate dateFim = LocalDate.parse(fim,formato);
+        if(dateFim.isBefore(dateInicio)){
+            return Collections.emptyList();
+        }else{
+            return movimentacaoRepository.findByDataBetween(dateInicio,dateFim);
         }
     }
 
