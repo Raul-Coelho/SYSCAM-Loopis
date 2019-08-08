0b92a6f5b9b78a3844561932854d5f36b91396a7
antonio <antoniofilhonerde@gmail.com>
2019-08-08 20:34:01 -0300

    metodo de busca por intervalo de data no repository de movimentacao

--- src/main/java/br/com/syscam/repository/MovimentacaoRepository.java
+++ src/main/java/br/com/syscam/repository/MovimentacaoRepository.java
@@ -2,6 +2,14 @@ package br.com.syscam.repository;
 
 import br.com.syscam.model.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao,Integer> {
+
+    @Query("SELECT m FROM Movimentacao m WHERE m.data BETWEEN ?1 AND ?2 ORDER BY m.data ASC")
+    public List<Movimentacao> findByDataBetween(LocalDate inicio, LocalDate fim);
 }
